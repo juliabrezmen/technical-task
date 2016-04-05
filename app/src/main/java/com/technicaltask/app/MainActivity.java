311c14c8ff0aa73e85e67969633889361a551c8c
Julia Danylyk <julia.danylyk@gmail.com>
2016-04-05 10:52:34 +0300

    Implemented UI.

--- app/src/main/java/com/technicaltask/app/MainActivity.java
+++ app/src/main/java/com/technicaltask/app/MainActivity.java
@@ -1,15 +1,56 @@
 package com.technicaltask.app;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_layout);
+
+        initToolBar();
+        initView();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        toolbar.inflateMenu(R.menu.main_menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_check:
+                // check
+                return true;
+            case R.id.action_search:
+                // search
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initToolBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void initView() {
+        FloatingActionButton btnAdd = (FloatingActionButton) findViewById(R.id.btn_add);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //  add user
+            }
+        });
     }
 }
